e0286bb4f51fff3cc44c26d73ee1ef51d3c367de
apri1f00ls <wkdduddn7321@gmail.com>
2025-11-13 12:20:44 +0900

    part2

--- diary_for_me/lib/timeline/widget/section_card.dart
+++ diary_for_me/lib/timeline/widget/section_card.dart
@@ -1,3 +1,4 @@
+import 'package:diary_for_me/common/ui_kit.dart';
 import 'package:flutter/material.dart';
 
 class SectionCard extends StatelessWidget {
@@ -37,3 +38,30 @@ class SectionCard extends StatelessWidget {
     );
   }
 }
+
+class SectionCard2 extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+  const SectionCard2({super.key, required this.title, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return contentsCard(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 8.0),
+          child: Text(title,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w700,
+              height: 1.2,
+              color: textTertiary
+            ),
+          ),
+        ),
+        ...children
+      ]
+    );
+  }
+}
+
